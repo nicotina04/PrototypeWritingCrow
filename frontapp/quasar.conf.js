ad4d40c7ab52b9b313aef9d66eb10e95ae7323d7
nicotina04 <devherdson@gmail.com>
2021-02-01 16:04:14 +0900

    Fixed product info

--- frontapp/quasar.conf.js
+++ frontapp/quasar.conf.js
@@ -117,9 +117,9 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'WhaleNoteMaster',
-        short_name: 'WhaleNoteMaster',
-        description: 'A quasar product of WhaleNote',
+        name: 'WrintingCrow',
+        short_name: 'WritingCrow',
+        description: 'A sample product of WritingCrow',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
@@ -184,7 +184,7 @@ module.exports = function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'whalenote_master_alpha'
+        appId: 'WritingCrow_alpha'
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
